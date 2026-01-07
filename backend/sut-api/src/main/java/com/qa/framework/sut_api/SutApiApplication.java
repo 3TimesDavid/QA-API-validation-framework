5264513ce4cddb359c59df9ea429a38af49058b8
Davidmapa01 <damapa01@alumnatflorida.es>
2026-01-07 20:12:46 +0100

    Add initial Spring Boot backend (SUT)

---
+++ backend/sut-api/src/main/java/com/qa/framework/sut_api/SutApiApplication.java
@@ -0,0 +1,13 @@
+package com.qa.framework.sut_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SutApiApplication.class, args);
+	}
+
+}
---
+++ backend/sut-api/src/test/java/com/qa/framework/sut_api/SutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.framework.sut_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
